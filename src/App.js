fcb2265ef943b1b0d70a198787531c1afe6beddb
pzey2011 <peymanzeynali@yahoo.com>
2020-08-04 00:21:26 +0430

    delete some extras

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import React , {useEffect , useState , useContext} from 'react';
+import React from 'react';
 import DeviceList from './DeviceList';
 import EditForm from './EditForm';
-import {DeviceContext,editDisplayContext,selectedDeviceContext} from './DeviceContext';
 
 function App() {
-  
-  const [devices,setDevices] = useContext(DeviceContext); 
-  const [editDisplay,setEditDisplay] = useContext(editDisplayContext); 
-   const [selectedDevice,setSelectedDevice] =useContext(selectedDeviceContext); 
-
- 
   return (
     <React.Fragment>
       <DeviceList />
